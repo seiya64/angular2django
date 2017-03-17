2f48c1a84e118e0bc8a5f8394321d5745fc04d9a
javi rodriguez <seiyadesagitario@gmail.com>
2017-03-17 01:44:26 +0100

    Updated .gitignore

---
+++ heroesFrontEnd/server.ts
@@ -0,0 +1,18 @@
+﻿var port = process.env.port || 1337
+var express = require('express');
+var app = express();
+var path = require('path');
+
+var __projectRoot = __dirname + '/src';
+var __nodeModules = __dirname + '/node_modules';
+
+app.use(express.static(__projectRoot));
+app.use('/node_modules', express.static(__nodeModules));
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__projectRoot + '/index.html'));
+});
+
+app.listen(port, function () {
+    console.log('Server up and running on http://localhost:' + port + '/');
+});
