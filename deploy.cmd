c553e81b2ef0cde8fec9bbb79364810edfa01dcd
javi rodriguez <seiyadesagitario@gmail.com>
2017-03-18 09:22:27 +0100

    Updated deploy script in order to sync only the node app

--- deploy.cmd
+++ deploy.cmd
@@ -88,6 +88,11 @@ goto :EOF
 :Deployment
 echo Handling node.js deployment.
 
+:: 0. Get only the node app
+IF DEFINED DEPLOYMENT_SOURCE (
+  Set DEPLOYMENT_SOURCE = "%DEPLOYMENT_SOURCE%\heroesFrontEnd\src"
+)
+
 echo "%DEPLOYMENT_SOURCE%"
 echo "%DEPLOYMENT_TARGET%"
 
