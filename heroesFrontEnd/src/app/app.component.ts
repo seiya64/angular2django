58cb85ea94b09d945e9edf17e6acf9268ca0ff93
javi rodriguez <seiyadesagitario@gmail.com>
2017-03-19 20:34:38 +0100

    More complex components in the main view. Added angular material

--- heroesFrontEnd/src/app/app.component.ts
+++ heroesFrontEnd/src/app/app.component.ts
@@ -1,7 +1,47 @@
 ﻿import { Component } from '@angular/core';
 
+export class Fort {
+    id: number;
+    name: string;
+    beacons: Array<Beacon>;
+}
+
+export class Beacon {
+    id: number;
+    name: string;
+    isBurning: boolean;
+}
+
+const BEACONS: Beacon[] = [
+    {
+        id: 1,
+        name: 'Pepito',
+        isBurning: true
+    },
+    {
+        id: 1,
+        name: 'Juaninito',
+        isBurning: false
+    },
+    {
+        id: 1,
+        name: 'Jaimito',
+        isBurning: false
+    }
+];
+
 @Component({
     selector: 'my-app',
-    template: `<h1>Hello {{name}}</h1>`
+    templateUrl: 'app/app.component.html'
 })
-export class AppComponent { name = 'Angular'; }
+
+export class AppComponent {
+    title = 'Almenaras';
+    forts: Fort[] = [
+        {
+            id: 1,
+            name: 'Camaradas del metal',
+            beacons: BEACONS
+        }
+    ];
+}
--- heroesFrontEnd/src/app/app.module.ts
+++ heroesFrontEnd/src/app/app.module.ts
@@ -1,9 +1,10 @@
 ﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { MaterialModule } from '@angular/material';
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [BrowserModule, MaterialModule],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })
